e9bd1f222f22f63f7da3312cc77ba7a689dd2206
abujafar01 <126306175+abujafar01@users.noreply.github.com>
2023-11-29 02:44:06 +0600

    Add files via upload

--- web.php
+++ web.php
@@ -1,1 +1,26 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+
+
+
+
+
+Route::get('/user/{id}/{name}',function(){
+    return view('user.welcome');
+})->where(['id=>'[0-9]+','name'=>'[A-Za-z]+']);
+
+Route::post('post_route_check',function(){
+    return view('user.welcome');
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('{type}', function ($type) {
+        $user_list = [1,2,3,4,5,6];
+        return view('admin.welcome',compact('type',$type,'user_list',$user_list));
+    });
+    Route::get('profile/{name}, function ($name) {
+        $user_list = [1,2,3,4,5,6];
+        return view('admin.profile',compact('name',$name,'user_list',$user_list));
+    });
+});
